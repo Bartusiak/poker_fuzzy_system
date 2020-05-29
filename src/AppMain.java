f585e3f2ce87c8ecd919e9b6e5419f6e4cb6a56f
Bartusiak <bsk69@o2.pl>
2020-05-29 17:27:37 +0200

    Added application to calculate type of player poker

--- src/AppMain.java
+++ src/AppMain.java
@@ -4,21 +4,25 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class AppMain extends Application {
 
 
     @Override
     public void start(Stage mainStage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("view/scene_player_view/scene_player_type.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/player_type_view.fxml"));
         Scene scene = new Scene(root);
-        scene.getStylesheets().add(getClass().getResource("view/scene_player_view/stylesheet.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("view/stylesheet.css").toExternalForm());
+
+
         mainStage.setTitle("CheckPlayerType");
         mainStage.setScene(scene);
         mainStage.show();
         //mainStage.setFullScreen(true);
 
     }
+
 
     public static void main(String[] args){
         launch();
---
+++ src/view/PlayerTypeController.java
@@ -0,0 +1,71 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.FunctionBlock;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+import net.sourceforge.jFuzzyLogic.rule.Variable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PlayerTypeController implements Initializable{
+
+    @FXML
+    private TextField vpipTextField;
+
+    @FXML
+    private TextField pfrTextField;
+
+    @FXML
+    private void handleButton(javafx.event.ActionEvent actionEvent) {
+        if(vpipTextField != null && pfrTextField != null){
+            System.out.println("START");
+            String fileName = "fcl/player_type.fcl";
+            FIS fis = FIS.load(fileName,true);
+            if (fis == null){
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Load File");
+                alert.setHeaderText("Something wrong with load the file");
+                alert.setContentText("Can't load the file: " + fileName + ".");
+                return;
+            }
+            FunctionBlock functionBlock = fis.getFunctionBlock(null);
+            JFuzzyChart.get().chart(functionBlock);
+            try {
+                float vpip = Float.valueOf(vpipTextField.getText());
+                float pfr = Float.valueOf(pfrTextField.getText());
+
+                fis.setVariable("vpip", vpip);
+                fis.setVariable("pfr", pfr);
+
+                fis.evaluate();
+
+                Variable var = functionBlock.getVariable("type");
+                JFuzzyChart.get().chart(var, var.getDefuzzifier(), true);
+                //System.out.println(fis);
+                //System.out.println("Type: " + fis.getVariable("type").getValue());
+                //System.out.println("Type: " + fis.getVariable("type"));
+            }
+            catch (NumberFormatException e){
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Inputs errror");
+                alert.setHeaderText("Something wrong with inputs - VPIP and PFR");
+                alert.setContentText(e.getMessage());
+            }
+
+
+
+            System.out.println("STOP");
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
